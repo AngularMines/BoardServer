ee6d9c1fe076aa6a3d8331bee69f7d1643087ef0
Alex Revell & CaraHill <pair+alexrevell.carahill@devbootcamp.com>
2015-04-30 14:48:50 +1200

    Created more methods for the Game class.

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,13 +8,24 @@ class Game
     @directions = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]]
   end
 
-  def find_neighbours
-    @board.each_with_index
+
+   def total_mines
+    @board.each_with_index {|row, r| row.each_with_index {|col, c| count_neighbour_mines(r,c) }}
+   end
+
+
+  def count_neighbour_mines(r,c)
+    unless @board[r][c].mine
+      @directions.each {|i,j| @board[r][c].neighbour_mines += 1 if @board[r + i][c + j].mine? && in_bounds?(r+i,c+j) }
+    end
   end
 
   def mine?(r,c)
     @board[r][c].mine
   end
 
+  def in_bounds?(r,c)
+    r.between?(0,@size-1) && c.between?(0,@size-1)
+  end
 
 end
